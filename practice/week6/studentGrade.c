710d1ad1a80364f9602b2f6ffc5558dbc49cdaff
dbsdk1 <spkwnf081620@naver.com>
2023-10-11 16:51:38 +0900

    Week6. 배열

---
+++ practice/week6/studentGrade.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define STUDENTS 5  // 컴파일 될 때 상수값 정의됨
+
+void classifyStudents(int scores[], char targetGrade) {
+	printf("학생 성적 분류:\n");
+	char grade = "";
+	for (int i = 0; i < STUDENTS; i++) {  // i가 0부터 시작하고 한번 돌고나면 1씩 증가, i가 5보다 작을때까지 반복
+		if (scores[i] >= 90) {  // 배열, 만약 i에 0 들어감 -> 배열의 크기는 0이다.
+			grade = 'A';  // 만약 scores[i]가 90보다 크거나 작으면 성적은 A이다.
+		}
+		else if (scores[i] >= 80) {
+			grade = 'B';
+		}
+		else if (scores[i] >= 70) {
+			grade = 'C';
+		}
+		else if (scores[i] >= 60) {
+			grade = 'D';
+		}
+		else {
+			grade = 'F'; 
+		}
+		if (targetGrade == grade) {
+			printf("%d 학생은 %c 점수를 받았습니다.", i + 1, targetGrade);  // targetGrade와 grade가 같으면 출력, %d에는 i+1의 값이 넣어지고 %c에는 targetGrade 넣어짐.
+
+		}
+	}
+
+}
+int main() {
+	int scores[STUDENTS];
+
+	for (int i = 0; i < STUDENTS; i++) { 
+		printf("학생 %d의 성적을 입력하세요: ", i + 1);  // 학생 1, 2, 3 이렇게 커지며 출력됨.
+		scanf_s("%d", &scores[i]);
+
+	}
+
+	char ch = getchar();  // 버퍼 임시 저장 변수, 엔터지우기 위해
+
+	char target;  // char형
+	printf("특정 점수 (A,B,C,D,F)를 입력하시오: ");
+	scanf_s("%c", &target, 1);  // 사용자로부터 값 입력받음
+
+	classifyStudents(scores, target);
+
+	return 0;
+}
